cda00134fd842e8bbdcc35ec14ce39cd34d28ec9
Ronit <ronitbajaj05@gmail.com>
2025-04-19 15:23:17 +0530

    7th

--- src/main/java/org/ncu/movie_app/entities/Genre.java
+++ src/main/java/org/ncu/movie_app/entities/Genre.java
@@ -1,14 +1,9 @@
 package org.ncu.movie_app.entities;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "genres")
@@ -23,20 +18,7 @@ public class Genre {
 
     @ManyToMany(mappedBy = "genres", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JsonBackReference("movie-genre")
-//    @JsonIgnoreProperties("genres")
     private List<Movie> movies = new ArrayList<>();
-//
-//    @JsonProperty("movies") // Custom serialization
-//    public List<Map<String, Object>> getMovieList() {
-//        return movies.stream()
-//                .map(movie -> {
-//                    Map<String, Object> map = new HashMap<>();
-//                    map.put("movieId", movie.getMovieId());
-//                    map.put("movieName", movie.getMovieName());
-//                    return map;
-//                })
-//                .collect(Collectors.toList());
-//    }
 
     // Constructors, getters, setters
     public Genre() {}
@@ -70,13 +52,11 @@ public class Genre {
         this.movies = movies;
     }
 
-    // Helper method to add movie
     public void addMovie(Movie movie) {
         movies.add(movie);
         movie.getGenres().add(this);
     }
 
-    // Helper method to remove movie
     public void removeMovie(Movie movie) {
         movies.remove(movie);
         movie.getGenres().remove(this);
